49fccff68e818b204d80cf66efc4254e2cd85678
anast-ananko <anast.ananko@gmail.com>
2021-08-07 21:20:07 +0300

    feat: complete typical-arrays-problems task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!array || !array.length) return 0;
+  return array.reduce((acc, cur) => cur < acc ? cur : acc);
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array || !array.length) return 0;
+  return array.reduce((acc, cur) => cur > acc ? cur : acc);
 }
 
+
 exports.avg = function avg (array) {
-  return 0;
+  if (!array || !array.length) return 0;
+  return array.reduce((acc, cur) => acc + cur) / array.length;
 }
